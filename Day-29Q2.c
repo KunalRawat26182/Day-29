bf08005d67065f24a6fa765b0fd10cbb187e1a34
Kunal Rawat <Kunal.26182@stu.upes.ac.in>
2025-11-10 21:56:40 +0530

    Add files via upload

---
+++ Day-29Q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100];
+    int max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = min = arr[0]; // initialize both with first element
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Max=%d, Min=%d\n", max, min);
+
+    return 0;
+}
